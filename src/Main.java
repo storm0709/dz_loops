da206c36fc44b579366e77197e18c49c808fda9c
storm0709 <anna.ponomarenko0709@gmail.com>
2023-04-10 17:55:51 +0200

    Revamped to For Solution

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main (String [] args) {
+     /*  int i = 6;
+        while (i > 0 && i <= 6) {
+            System.out.print (i + " ");
+
+            int j = i;
+            while(j > 0 && j <= 6) {
+                System.out.print("#");
+                j--;
+            }
+
+            i--;
+           System.out.println ();
+        }*/
+
+        for (int i = 6; i > 0 && i <= 6; i--) {
+            System.out.print (i + " ");
+            for (int j = i; j > 0 && j <= 6; j--)
+                System.out.print("#");
+            System.out.println ();
+        }
+    }
+}
