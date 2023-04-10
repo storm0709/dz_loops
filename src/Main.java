4bdf9ba56272415a41a7082c1511bd4cd800ce69
storm0709 <anna.ponomarenko0709@gmail.com>
2023-04-10 17:32:31 +0200

    My commit For Solution

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String [] args) {
-        int i = 6;
+       /* int i = 6;
         while (i > 0 && i <= 6) {
             System.out.print (i + " ");
 
@@ -11,6 +11,13 @@ public class Main {
             }
 
             i--;
+            System.out.println ();
+        }*/
+
+        for (int i = 6; i > 0 && i <= 6; i--) {
+            System.out.print (i + " ");
+            for (int j = i; j > 0 && j <= 6; j--)
+                System.out.print("#");
             System.out.println ();
         }
     }
